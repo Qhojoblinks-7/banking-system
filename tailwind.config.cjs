e3e38bc39d2a1b209451a1677e7bbcdf6a8f71d8
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2025-02-17 16:19:01 +0000

    fix: add axios dependency and configure Tailwind CSS purge

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
