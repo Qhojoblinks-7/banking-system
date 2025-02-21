35a7bce50ada7880d0e84d35d2766420dbc402de
Immanuel Quansah Eshun <124244985+Qhojoblinks-7@users.noreply.github.com>
2025-02-21 11:38:55 +0000

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './components/UserContext';
-import './index.css'; // Make sure to import your CSS file with the styles
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
